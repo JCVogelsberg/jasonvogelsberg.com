bd1acdefecd8c6a8dbc436a13d5e5c71328bb749
Jason Vogelsberg <JCVogelsberg@MacAttack.local>
2016-08-23 11:41:15 -0700

    Latest updates to JasonVogelsberg.com

--- js/scripts.js
+++ js/scripts.js
@@ -10,50 +10,7 @@ $(document).ready(function() {
 
 
 
-
-// ( function( $ ) {
-//     // Init Skrollr
-//     var s = skrollr.init({
-//         render: function(data) {
-//             //Debugging - Log the current scroll position.
-//             //console.log(data.curTop);
-//         }
-//     });
-// } )( jQuery );
-
-
-
-
 // // Redirect Page Function
 // var redirect = function(x) {
 //   window.location.assign(x)
 // };
-
-
-// $(document).ready(function() {
-//   $('.tile').each(function() {
-//     $.data(this, 'dialog',
-//       $(this).next('.summary').dialog({
-//         autoOpen: false,
-//         title: null,
-//         width: 400,
-//         modal: true,
-//         //position: 'center',
-//         // height: 400,
-//         draggable: false
-//       })
-//     );
-//   }).mouseenter(function() {
-//     $.data(this, 'dialog').dialog('open');
-//     return false;
-//    }).mouseleave(function() {
-//     $.data(this, 'dialog').dialog('close');
-//     return false;
-//   });
-
-//   $(".summary").position({
-//     my: "center",
-//     at: "center",
-//     of: target
-//   });
-// });
